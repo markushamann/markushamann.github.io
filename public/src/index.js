6d55df4bc8c33767703588b4f733ee072c1ce2c8
Markus Hamann <mhamann@edu.uwaterloo.ca>
2022-08-08 12:23:03 -0400

    firebase links

---
+++ public/src/index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAgeppivfKBSDQpF6IY_ny9mhiasHGPpUI",
+    authDomain: "markus-hamann-personal-website.firebaseapp.com",
+    projectId: "markus-hamann-personal-website",
+    storageBucket: "markus-hamann-personal-website.appspot.com",
+    messagingSenderId: "341200013012",
+    appId: "1:341200013012:web:74ef4c47bd7f4851215d41",
+    measurementId: "G-3P6TZ8H2J9"
+};
+
+const app = initializeApp(firebaseConfig);
+
